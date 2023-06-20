739a8bc2a1fd3aa62ca064f5afe4f18043a305df
Linton <pereira.linton@gmail.com>
2023-06-20 23:59:05 +0530

    lec 4 without dynamic data update

--- src/index.js
+++ src/index.js
@@ -25,13 +25,17 @@ const Heading = () => {
   );
 }
 
-const ResCard = () => {
-  // const biryaniImage = "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.licious.in%2Fblog%2Fwp-content%2Fuploads%2F2022%2F06%2Fchicken-hyderabadi-biryani-01.jpg&tbnid=WDeZeD4Aq11VEM&vet=12ahUKEwjYqf-X4c__AhWCg9gFHcaMC9sQMygLegUIARCtAg..i&imgrefurl=https%3A%2F%2Fwww.licious.in%2Fblog%2Frecipe%2Fhyderabadi-chicken-biryani-recipe-2&docid=hhSRDS503Q7lYM&w=900&h=900&q=chicken%20biryani&ved=2ahUKEwjYqf-X4c__AhWCg9gFHcaMC9sQMygLegUIARCtAg";
+const resList = [{}, {}];
+
+const ResCard = (props) => {
+  const biryaniImage = "https://www.licious.in/blog/wp-content/uploads/2022/06/chicken-hyderabadi-biryani-01-750x750.jpg";
+  // const {resData} = props;
+  // const {name, avgRating, cuisines, costForTwo, deliveryTime} = resData?.data;
   return (
     <div className='res-card'>          
       <h3>Zaffran</h3>
       <div className='res-card-image-container'>
-        <img className='res-image' src={foodImage} alt='hotel-dish' />
+        <img className='res-image' src={biryaniImage} alt='hotel-dish' />
       </div>
       <h4>North Indian, Italian</h4>
       <h4>4.3 stars</h4>
@@ -45,6 +49,11 @@ const Body = () => {
     <div className='body'>
       <div className='search'>Search</div>
       <div className='res-container'>
+        {/* {
+          resList.map((restaurant) => {
+            <ResCard key={restaurant.data.id} resData={resList} />
+          })
+        } */}
         <ResCard />
         <ResCard />
         <ResCard />
